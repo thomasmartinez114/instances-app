92dd5a8ae7796540e8ac8b07e9f21418a1ac08a7
Thomas Martinez <thomas.martinez@igt.com>
2024-02-26 21:52:56 -0500

    backend working with postman and adding to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,46 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Welcome@123!!',
+  database: 'gnie',
+});
+
+app.get('/', (req, res) => {
+  res.json('hello this is the backend');
+});
+
+app.get('/instances', (req, res) => {
+  const q = 'SELECT * FROM instances';
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post('/instances', (req, res) => {
+  const q =
+    'INSERT INTO instances (`title`, `prodURL`, `prodAdmin`, `qaURL`, `qaAdmin`,  `image`) VALUES (?)';
+  const values = [
+    req.body.title,
+    req.body.prodURL,
+    req.body.prodAdmin,
+    req.body.qaURL,
+    req.body.qaAdmin,
+    req.body.image,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json('Instance has been created successfully.');
+  });
+});
 
 app.listen(8800, () => {
   console.log('Connected to backend!');
